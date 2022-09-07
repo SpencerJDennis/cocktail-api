a34efc877a7ecfeab0d1cb000d4da665fd451112
Spencer Dennis <sjdennis2113@gmail.com>
2022-09-07 15:18:27 -0700

    add cocktail-service and the class RandomCocktail
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>

---
+++ src/js/cocktail-service.js
@@ -0,0 +1,19 @@
+export class RandomCocktail {
+  static getRandomCocktail() {
+    return promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://www.thecocktaildb.com/api/json/v1/1/random.php`;
+
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response]);
+        } else {
+          reject([this, response]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
