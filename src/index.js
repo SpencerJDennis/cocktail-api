29a730578ba22b16292768b24328d9f3f8a18dad
Spencer Dennis <sjdennis2113@gmail.com>
2022-09-08 15:52:10 -0700

    finished project
    Co-authored-by: Skylan Lew <skylanslew@gmail.com>
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { RandomCocktail } from './js/cocktail-service.js'
+import { Search } from './js/cocktail-service.js'
 
 function getRandomCocktail() {
   let promise = RandomCocktail.getRandomCocktail();
@@ -9,14 +10,41 @@ function getRandomCocktail() {
   });
 }
 
+async function byIngredient(ingredient) {
+  const response = await Search.byIngredient(ingredient);
+  console.log(response)
+  if (response.drinks) {
+    printElements2(response, ingredient);
+  } else {
+    printError2(response, ingredient);
+  }
+}
+
+function printElements2(response, ingredient){
+  const answerDiv = document.querySelector('#answer');
+  const printList = document.createElement("ul");
+  response.drinks.forEach(function(element){
+    let li = document.createElement("li");
+    li.append(element.strDrink);
+    printList.append(li);
+  });
+  answerDiv.append(printList);
+  console.log(response + " " +  ingredient);
+}
+
+function printError2(error, ingredient){
+  document.querySelector('#answer').innerText = `There was an error accessing the drinks for ${ingredient}: 
+  ${error}.`;
+}
+
 function printElements(cocktail) {
   console.log(cocktail)
   let answer = document.getElementById('answer');
   const displayImage = document.createElement("img");
   displayImage.setAttribute("src", `${cocktail[0].drinks[0].strDrinkThumb}`);
   displayImage.setAttribute("height", "100px");
-  answer.append(`Your random cocktail is called ${cocktail[0].drinks[0].strDrink}.`);
   answer.append(displayImage);
+  answer.append(`Your random cocktail is called ${cocktail[0].drinks[0].strDrink}.`);
 
   const ingredientsArray = [cocktail[0].drinks[0].strIngredient1, cocktail[0].drinks[0].strIngredient2, cocktail[0].drinks[0].strIngredient3,cocktail[0].drinks[0].strIngredient4, cocktail[0].drinks[0].strIngredient5,cocktail[0].drinks[0].strIngredient6, cocktail[0].drinks[0].strIngredient7,cocktail[0].drinks[0].strIngredient8, cocktail[0].drinks[0].strIngredient9, cocktail[0].drinks[0].strIngredient10, cocktail[0].drinks[0].strIngredient11, cocktail[0].drinks[0].strIngredient12,cocktail[0].drinks[0].strIngredient13, cocktail[0].drinks[0].strIngredient14,cocktail[0].drinks[0].strIngredient15];
 
@@ -29,9 +57,9 @@ function printElements(cocktail) {
       measureArray.splice(i, 15); //might need to separate out later
     }
   }
-  console.log(ingredientsArray + "ingredients");
-  console.log(measureArray + "measure");
-
+  answer.append(`Your ingredients are: ${ingredientsArray}.`);
+  answer.append(`Your measurements are: ${measureArray}.`);
+  answer.append(`Instructions: ${cocktail[0].drinks[0].strInstructions}`);
 }
 
 function printError(error) {
@@ -44,4 +72,13 @@ function handleFormSubmission() {
   getRandomCocktail();
 }
 
-document.querySelector('form').addEventListener("click", handleFormSubmission);
+function notHandleFormSubmissionThanksWow() {
+  let userInput = document.getElementById("search-ingredient").value;
+  let answer = document.getElementById('answer');
+  answer.innerHTML = "";
+  byIngredient(userInput);
+}
+
+document.querySelector('#cocktail-btn').addEventListener("click", handleFormSubmission);
+
+document.querySelector('#ingredient-search-btn').addEventListener("click", notHandleFormSubmissionThanksWow);
--- src/js/cocktail-service.js
+++ src/js/cocktail-service.js
@@ -18,3 +18,20 @@ export class RandomCocktail {
   }
 }
 
+export class Search {
+  static async byIngredient(ingredient) {
+    try {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}
+        ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
+
